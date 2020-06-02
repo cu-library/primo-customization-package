d38cf2ee848ba3599e731ac53979d50ba00026d4
Kevin Bowrin <kevinbowrin@cunet.carleton.caw>
2020-06-02 19:47:10 -0400

    Rework with dev env, add HT full text links

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import 'primo-explore-hathitrust-availability';
+(function () {
+    'use strict';
+    var app = angular.module('viewCustom', ['angularLoad', 'hathiTrustAvailability']);
+    // Include HathiTrust full text links.
+    app.component('prmSearchResultAvailabilityLineAfter', {
+        template: '<hathi-trust-availability ignore-copyright="true" entity-id="http://cufed.carleton.ca/adfs/services/trust"></hathi-trust-availability>'
+    })
+    // Change the default RIS export character encoding to UTF-8
+    app.component('prmExportRisAfter', {
+        bindings: { parentCtrl: '<' },
+        controller: function() { this.parentCtrl.encodingVal = 'UTF-8'; },
+        template: ''
+    });
+})();
