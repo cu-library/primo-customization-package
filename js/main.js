3bbdfec7a4279988510d0cc74d4e41432bae6207
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2021-11-16 10:17:56 -0500

    Add period to comment.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ import 'primo-explore-hathitrust-availability';
     app.component('prmSearchResultAvailabilityLineAfter', {
         template: '<hathi-trust-availability msg="Full Text Available Online at HathiTrust" entity-id="http://cufed.carleton.ca/adfs/services/trust"></hathi-trust-availability>'
     })
-    // Change the default RIS export character encoding to UTF-8
+    // Change the default RIS export character encoding to UTF-8.
     app.component('prmExportRisAfter', {
         bindings: { parentCtrl: '<' },
         controller: function() { this.parentCtrl.encodingVal = 'UTF-8'; },
