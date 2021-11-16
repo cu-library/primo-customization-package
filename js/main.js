9829cf052aa06e069c1f46d661ee49bb8c14e6a5
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2021-11-16 11:55:43 -0500

    Add warning for kiosk users

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ import 'primo-explore-hathitrust-availability';
         controller: function() { this.parentCtrl.encodingVal = 'UTF-8'; },
         template: ''
     });
+    // Add a warning for kiosk users.
+    app.component('kioskWarning', {
+	    template: '<div layout="row" layout-align="center center" class="kiosk-warning-wrapper"><div flex-sm="80" flex-md="70" flex-lg="60" flex-xl="60" class="kiosk-warning-content" flex="80" layout="row"><md-icon md-svg-icon="primo-ui:error-outline"></md-icon><div class="kiosk-warning-text"><span class="kiosk-warning-label">Notice: </span>These Omni stations only search physical items located in the Carleton Library.<br>For full search functionality to find e-resources and more, please use the regular Omni site via the Library website.</div></div></div>'
+    });
+    app.component('prmSearchBarAfter', {
+        bindings: {parentCtrl: '<'},
+        template: '<kiosk-warning></kiosk-warning>'
+    });
 })();
 (function() {
     'use strict';
