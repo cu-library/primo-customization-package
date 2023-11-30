285a13c6473a13868269f2ff6ebc2719a8693cc3
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2023-11-30 15:13:00 -0500

    Hide Rapido block for items with RSV item location.

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ import 'primo-explore-hathitrust-availability';
     var app = angular.module('viewCustom', ['angularLoad', 'hathiTrustAvailability']);
     // Include HathiTrust full text links.
     app.component('prmSearchResultAvailabilityLineAfter', {
-        template: '<hathi-trust-availability msg="Full Text Available Online at HathiTrust" entity-id="http://cufed.carleton.ca/adfs/services/trust"></hathi-trust-availability>'
+        template: `<hathi-trust-availability msg="Full Text Available Online at HathiTrust" entity-id="http://cufed.carleton.ca/adfs/services/trust"></hathi-trust-availability>`
     });
+
     // Change the default RIS export character encoding to UTF-8.
     app.component('prmExportRisAfter', {
         bindings: { parentCtrl: '<' },
@@ -13,12 +14,16 @@ import 'primo-explore-hathitrust-availability';
         template: ''
     });
 
+    // Add Carleton's Acceptable Use Policy link.
+    app.component('prmServiceHeaderAfter', {
+        bindings: {parentCtrl: '<'},
+        template: `<carleton-acceptable-use-policy-link parent-ctrl="$ctrl.parentCtrl"></carleton-acceptable-use-policy-link>`
+    });
     app.component('carletonAcceptableUsePolicyLink', {
         bindings: {parentCtrl: '<'},
         controller: 'carletonAcceptableUsePolicyLinkController',
         template: `<a ng-if="$ctrl.online()" href="https://library.carleton.ca/about/policies/electronic-resources-acceptable-use-policy">Electronic Resources Acceptable Use Policy <prm-icon icon-type="svg" svg-icon-set="primo-ui" external-link="" icon-definition="open-in-new"></prm-icon></a>`
     });
-
     app.controller('carletonAcceptableUsePolicyLinkController', [function () {
         var vm = this;
         vm.online = online;
@@ -27,10 +32,35 @@ import 'primo-explore-hathitrust-availability';
         }
     }]);
 
-    app.component('prmServiceHeaderAfter', {
-        bindings: {parentCtrl: `<`},
-        template: `<carleton-acceptable-use-policy-link parent-ctrl="$ctrl.parentCtrl"></carleton-acceptable-use-policy-link>`
+    // Hide Rapido block for reserves items.
+    app.component('prmServiceNgrsAfter', {
+        bindings: {parentCtrl: '<'},
+        template: `<carleton-hide-rapido-for-reserves-items parent-ctrl="$ctrl.parentCtrl">
+                   </carleton-hide-rapido-for-reserves-items>`
     });
+    app.component('carletonHideRapidoForReservesItems', {
+        bindings: {parentCtrl: '<'},
+        controller: 'carletonHideRapidoForReservesItemsController',
+        template: `<span ng-if="$ctrl.hide()"></span>`
+    });
+    app.controller('carletonHideRapidoForReservesItemsController', [function() {
+        var vm = this;
+        vm.hide = hide;
+        function hide(){
+            if (vm.parentCtrl.hasOwnProperty('item') &&
+                vm.parentCtrl.item.hasOwnProperty('delivery') &&
+                vm.parentCtrl.item.delivery.hasOwnProperty('holding') &&
+                Array.isArray(vm.parentCtrl.item.delivery.holding)) {
+               vm.parentCtrl.item.delivery.holding.forEach(function(itemHolding) {
+                   if (itemHolding.libraryCode === "MACODRUM" &&
+                       itemHolding.subLocationCode === "rsv") {
+                       vm.parentCtrl._almaRSServiceAvailable = "false";
+                   }
+               });
+            }
+            return false;
+        }
+    }]);
 })();
 (function() {
     'use strict';
